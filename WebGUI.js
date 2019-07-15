51db14c709c8431e0bdef893b63cedddfc18a46e
Mason Mackaman <masondeanm@aol.com>
2019-07-15 17:29:20 -0400

    Initial commit

---
+++ WebGUI.js
@@ -0,0 +1,38 @@
+const webGui = {
+    popup(element, refElement = undefined, xe = .5, xr = .5, ye = .5, yr = .5, dim = '55') {
+        let background = document.createElement('div')
+        Object.assign(background.style, {
+            position: 'fixed',
+            background: '#000000' + dim,
+            left: 0,
+            top: 0,
+            width: '100%',
+            height: '100%',
+        })
+
+        background.addEventListener('click', function(e) {
+            if (!e.composedPath().includes(element)) this.remove();
+        });
+
+        Object.assign(element.style, {
+            position: 'relative',
+            margin: 0,
+            visibility: 'hidden',
+            'box-shadow': '0 0 10px'
+        })
+
+        background.appendChild(element);
+        document.body.appendChild(background);
+
+        let dims = (refElement || background).getBoundingClientRect();
+        (new MutationObserver(()=> background.remove())).observe(background, {childList: true});
+        Object.assign(element.style, {
+            left: dims.left + xr * dims.width - xe * element.offsetWidth + 'px',
+            top: dims.top + yr * dims.height - ye * element.offsetHeight + 'px',
+    		visibility: 'visible',
+        })
+    },
+}
+
+export default webGui;
+export {webGui};
