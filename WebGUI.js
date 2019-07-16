a256bce12777ba9356f7b828990b634d8545669f
Mason Mackaman <masondeanm@aol.com>
2019-07-16 12:46:29 -0400

    Change all whitespace to tabs
    Unbeknownst to me, I was mixing up tabs and spaces because Atom made the
    spaces seem like tabs.

--- WebGUI.js
+++ WebGUI.js
@@ -1,45 +1,45 @@
 const webGui = {
-    popup(element, refElement = undefined, xe = .5, xr = .5, ye = .5, yr = .5, dim = '55') {
-        let background = document.createElement('div')
-        Object.assign(background.style, {
-            position: 'fixed',
-            background: '#000000' + dim,
-            left: 0,
-            top: 0,
-            width: '100%',
-            height: '100%',
-        })
+	popup(element, refElement = undefined, xe = .5, xr = .5, ye = .5, yr = .5, dim = '55') {
+		let background = document.createElement('div')
+		Object.assign(background.style, {
+			position: 'fixed',
+			background: '#000000' + dim,
+			left: 0,
+			top: 0,
+			width: '100%',
+			height: '100%',
+		})
 
-        background.addEventListener('click', function(e) {
-            if (!e.composedPath().includes(element)) this.remove();
-        });
+		background.addEventListener('click', function(e) {
+			if (!e.composedPath().includes(element)) this.remove();
+		});
 
-        Object.assign(element.style, {
-            position: 'relative',
-            margin: 0,
-            visibility: 'hidden',
-            'box-shadow': '0 0 10px'
-        })
+		Object.assign(element.style, {
+			position: 'relative',
+			margin: 0,
+			visibility: 'hidden',
+			'box-shadow': '0 0 10px'
+		})
 
-        background.appendChild(element);
-        document.body.appendChild(background);
+		background.appendChild(element);
+		document.body.appendChild(background);
 
-        let dims = (refElement || background).getBoundingClientRect();
-        (new MutationObserver(()=> background.remove())).observe(background, {childList: true});
-        Object.assign(element.style, {
-            left: dims.left + xr * dims.width - xe * element.offsetWidth + 'px',
-            top: dims.top + yr * dims.height - ye * element.offsetHeight + 'px',
-    		visibility: 'visible',
-        })
-    },
-    transWait(elem) {
-        return new Promise(resolve => {
-            elem.addEventListener('transitionend', function eh() {
-                elem.removeEventListener('transitionend', eh);
-                resolve();
-            });
-        })
-    },
+		let dims = (refElement || background).getBoundingClientRect();
+		(new MutationObserver(()=> background.remove())).observe(background, {childList: true});
+		Object.assign(element.style, {
+			left: dims.left + xr * dims.width - xe * element.offsetWidth + 'px',
+			top: dims.top + yr * dims.height - ye * element.offsetHeight + 'px',
+			visibility: 'visible',
+		})
+	},
+	transWait(elem) {
+		return new Promise(resolve => {
+			elem.addEventListener('transitionend', function eh() {
+				elem.removeEventListener('transitionend', eh);
+				resolve();
+			});
+		})
+	},
 }
 
 export default webGui;
