e87797be63787056147bf134ed33e30b38e5662e
Mason Mackaman <masondeanm@aol.com>
2019-08-30 01:01:48 -0400

    Add boxShadow option

--- WebGUI.js
+++ WebGUI.js
@@ -5,26 +5,33 @@ const webGui = {
 			child.remove();
 		}
 	},
-	popup(element, refElement = undefined, {xe = .5, xr = .5, ye = .5, yr = .5, dim = 0x55} = {}) {
-		let background = document.createElement('div')
+	popup(element, refElement = undefined, {
+		xe = .5,
+		xr = .5,
+		ye = .5,
+		yr = .5,
+		dim = 0x55,
+		boxShadow = `0 0 10px`
+	} = {}) {
+		let background = document.createElement(`div`)
 		Object.assign(background.style, {
-			position: 'fixed',
-			background: '#000000' + dim.toString(16),
+			position: `fixed`,
+			background: `#000000` + dim.toString(16),
 			left: 0,
 			top: 0,
-			width: '100%',
-			height: '100%',
+			width: `100%`,
+			height: `100%`,
 		})
 
-		background.addEventListener('click', function(e) {
+		background.addEventListener(`click`, function(e) {
 			if (!e.composedPath().includes(element)) this.remove();
 		});
 
 		Object.assign(element.style, {
-			position: 'relative',
+			position: `relative`,
 			margin: 0,
-			visibility: 'hidden',
-			'box-shadow': '0 0 10px'
+			visibility: `hidden`,
+			'box-shadow': boxShadow
 		})
 
 		background.appendChild(element);
@@ -33,15 +40,15 @@ const webGui = {
 		let dims = (refElement || background).getBoundingClientRect();
 		(new MutationObserver(()=> background.remove())).observe(background, {childList: true});
 		Object.assign(element.style, {
-			left: dims.left + xr * dims.width - xe * element.offsetWidth + 'px',
-			top: dims.top + yr * dims.height - ye * element.offsetHeight + 'px',
-			visibility: 'visible',
+			left: dims.left + xr * dims.width - xe * element.offsetWidth + `px`,
+			top: dims.top + yr * dims.height - ye * element.offsetHeight + `px`,
+			visibility: `visible`,
 		});
 	},
 	transWait(elem) {
 		return new Promise(resolve => {
-			elem.addEventListener('transitionend', function eh() {
-				elem.removeEventListener('transitionend', eh);
+			elem.addEventListener(`transitionend`, function eh() {
+				elem.removeEventListener(`transitionend`, eh);
 				resolve();
 			});
 		})
